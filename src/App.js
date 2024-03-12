a0540b50d7c4b2c99e33bfaf5dccf3013847e8aa
Daniel Tilahun <tiledan2015@gmail.com>
2024-03-13 01:28:47 +0300

    complete Task_1_Building_Your_First_Card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./component/Task_1_Building_Your_First_Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="h-screen w-screen flex items-center justify-center bg-gray-900">
+      <Card />
+    </h1>
   );
 }
 
---
+++ src/component/Task_1_Building_Your_First_Card.js
@@ -0,0 +1,57 @@
+const Card = () => {
+  return (
+    <div className="h-[266px] w-[919px] rounded-[30px] border-[1px] bg-white p-[24px]">
+      <div className="flex h-[100%] w-[100%] gap-3">
+        <div className="h-[100%] w-[89px">
+          <img src="/logo.svg" className="h-[59px] w-[66px]" />
+        </div>
+        <div className="h-[100%] w-[755px] flex flex-col gap-[8px]">
+          <div class="font-epilogue font-semibold text-[20px] text-blue-900">
+            Social Media Assistant
+          </div>
+          <div class="flex w-[454px] h-[27px] gap-[8px] items-center">
+            <div class="font-epilogue font-normal text-[16px] text-blue-900">
+              Young Men Christians Association
+            </div>
+            <div class="w-[4px] h-[4px] flex items-center justify-center bg-[#7C8493] rounded-full"></div>
+            <div class="font-epilogue font-normal text-[16px] text-blue-900">
+              Addis Ababa, Ethiopia
+            </div>
+          </div>
+
+          <div class="w-[744px] h-[112px] font-epilogue font-normal text-base leading-25.6">
+            As a Social Media Assistant, you will work closely with the social
+            media manager or marketing team to execute social media strategies
+            and campaigns. You will be responsible for assisting in the creation
+            and scheduling of engaging content, monitoring social media
+            channels, and interacting with followers.
+          </div>
+
+          <div class="flex w-[242px] h-[31px] gap-[8px]">
+            <div class="bg-[#56CDAD] w-[76px] h-[31px] rounded-[80px] p-[6px] gap-[8px] flex justify-center items-center">
+              <div class="font-epilogue font-semibold text-[12px] text-blue-900">
+                In person
+              </div>
+            </div>
+
+            <div class="h-[100%] w-[1.5px] bg-gray-300"></div>
+
+            <div class="w-[76px] h-[31px] rounded-[80px] p-[6px] gap-[8px] flex justify-center items-center border border-[#FFB836]">
+              <div class="font-epilogue font-semibold text-[12px] text-[#FFB836]">
+                Education
+              </div>
+            </div>
+
+            <div class="w-[76px] h-[31px] rounded-[80px] p-[6px] gap-[8px] flex justify-center items-center border border-[#4640DE]">
+              <div class="font-epilogue font-semibold text-[12px] text-black">
+                IT
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
